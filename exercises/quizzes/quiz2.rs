c123e580d71b5b0d7528d5ee066725f1091c6766
pandey-prateek <pandey.prateek@outlook.com>
2024-12-26 17:04:23 +0530

    exercise

---
+++ exercises/quizzes/quiz2.rs
@@ -0,0 +1,72 @@
+// This is a quiz for the following sections:
+// - Strings
+// - Vecs
+// - Move semantics
+// - Modules
+// - Enums
+//
+// Let's build a little machine in the form of a function. As input, we're going
+// to give a list of strings and commands. These commands determine what action
+// is going to be applied to the string. It can either be:
+// - Uppercase the string
+// - Trim the string
+// - Append "bar" to the string a specified amount of times
+//
+// The exact form of this will be:
+// - The input is going to be a Vector of 2-length tuples,
+//   the first element is the string, the second one is the command.
+// - The output element is going to be a vector of strings.
+
+enum Command {
+    Uppercase,
+    Trim,
+    Append(usize),
+}
+mod my_module {
+    use super::Command;
+
+    // TODO: Complete the function as described above.
+    pub fn transformer(mut input: Vec<(String,Command)>) -> Vec<(String)> {
+       input.iter().map(|(k, v)| {
+            match v {
+                Command::Uppercase => (k.to_uppercase(),Command::Uppercase),
+                Command::Trim => (k.trim().to_string(),Command::Trim),
+                Command::Append(n) => (k.to_string()+&"bar".repeat(*n), Command::Append(*n)),
+            }
+        }).map(|(k,v)|k).collect()
+    }
+    
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::my_module::transformer;
+    // TODO: What do we need to import to have `transformer` in scope?
+    // use ???;
+    use super::Command;
+
+    #[test]
+    fn it_works() {
+        let input = vec![
+            ("hello".to_string(), Command::Uppercase),
+            (" all roads lead to rome! ".to_string(), Command::Trim),
+            ("foo".to_string(), Command::Append(1)),
+            ("bar".to_string(), Command::Append(5)),
+        ];
+        let output = transformer(input);
+
+        assert_eq!(
+            output,
+            [
+                "HELLO",
+                "all roads lead to rome!",
+                "foobar",
+                "barbarbarbarbarbar",
+            ]
+        );
+    }
+}
